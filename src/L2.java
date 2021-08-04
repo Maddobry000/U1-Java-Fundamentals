f66611aeef44f90fc58e6a4177e25592949515a1
maddobry000 <Maddobry000@warren.k12.in.us>
2021-08-04 12:52:27 -0400

    completed L1 and L2 notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,25 @@
+import java.sql.SQLOutput;
+
 public class L2 {
 
-    //add the main method header
+
+        public static void main(String[] args) {
+
+            double num1 = 3.4;
+            boolean num2 = true;
+            double num3 = 4.5;
+
+            String firstname = "George";
+            int age = 16;
+            System.out.println(firstname);
+            System.out.println(age);
+
+            String accountName = "Bryce";
+            int accountnumbeer = 123456789;
+            double balance = 123456789;
+
+            System.out.println(accountName);
+            System.out.println(accountnumbeer);
+            System.out.println(balance);
+        }
 }
