a2e319753828174099ae67f3a334678305049f1e
maddobry000 <Maddobry000@warren.k12.in.us>
2021-08-09 13:06:28 -0400

    L3-L5 notes

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,26 @@
 public class L4 {
+
+    public static void main(String[] args) {
+
+        int value = 5;
+
+        //Declare a variable - stating the datatype and identifier
+
+        double total;
+    String message ="HI";
+
+        //Assign a value
+        total = 6;
+
+
+        System.out.print(message + ", the value is" + total);
+
+        total = 10;
+        message ="Bye";
+        //Gravity = -10;     -- dis is bad
+
+        System.out.println(message + ", the value is" + total);
+
+
+        }
 }
